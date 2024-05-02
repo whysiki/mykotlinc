51847ee78262311b23dc7bd2fc0c77f0edae581e
whysiki <whysiki@proton.me>
2024-05-02 17:40:57 +0800

    11

--- test.bat
+++ test.bat
@@ -7,13 +7,9 @@ chcp 65001
 @REM python mykotlinc.py test.kt --params "-w --no-rebuild --configuration-cache --parallel --daemon --build-cache --max-workers 6" --clear
 @REM python mykotlinc.py --help
 
-python mykotlinc.py test.kt --params "-w --no-rebuild --configuration-cache --parallel --daemon --build-cache --max-workers 6" --clear
-
-mykotlinc.exe D:\KotlinCompiler\mykotlinc\test.kt --params "-w --no-rebuild --configuration-cache --parallel --daemon --build-cache --max-workers 6" --clear
-
-mykotlinc.exe D:\KotlinCompiler\mykotlinc\test.kt --clear --params "-w --no-rebuild --configuration-cache --parallel --daemon --build-cache --max-workers 6" 
-
-
-mykotlinc.exe test.kt --clear
-
-@REM mykotlinc.exe -h
+python mykotlinc.py test.kt --params "-w --no-rebuild --configuration-cache --parallel --daemon --build-cache --max-workers 6" --clear && ^
+python mykotlinc.py D:\KotlinCompiler\mykotlinc\test.kt --params "-w --no-rebuild --configuration-cache --parallel --daemon --build-cache --max-workers 6" --clear && ^
+mykotlinc.exe D:\KotlinCompiler\mykotlinc\test.kt --params "-w --no-rebuild --configuration-cache --parallel --daemon --build-cache --max-workers 6" --clear && ^
+mykotlinc.exe D:\KotlinCompiler\mykotlinc\test.kt --clear --params "-w --no-rebuild --configuration-cache --parallel --daemon --build-cache --max-workers 6" && ^
+mykotlinc.exe test.kt --clear && ^
+mykotlinc.exe -h
